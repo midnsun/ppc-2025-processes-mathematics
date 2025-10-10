7dba985b19d84ebfc2a9a5a1f78fafb62b35eee5
zagriadskov maksim <maksimzagradskrv50910@gmail.com>
2025-10-11 00:06:31 +0300

    Another task

---
+++ mpi/zagryadskov_m_max_by_column/include/matrix.h
@@ -0,0 +1,42 @@
+
+
+#include <algorithm>
+#include <utility>
+#include <vector>
+
+namespace zagryadskov_a_matrix_sum {
+    template <typename T>
+    class myMatrix : public ppc::core::Task {
+        std::vector<T> data;
+        size_t m;
+        size_t n;
+        public:
+        myMatrix (size_t _m, size_t _n) : m(_m), n(_n), data(_m * _n) {}
+        myMatrix (const myMatrix& copy): m(copy.m), n(copy.n), data(copy.data) {}
+        myMatrix (myMatrix&& mov): m(mov.m), n(mov.n), data(std::move(copy.data)) {}
+        const myMatrix& operator= (const myMatrix& copy) {
+            m = copy.m;
+            n = copy.n;
+            data = copy.data;
+            return *this;
+        }
+        const myMatrix& operator= (myMatrix&& mov) {
+            m = mov.m;
+            n = mov.n;
+            data = std::move(mov.data);
+            return *this;
+        }
+        std::vector<T>& getData() { 
+            return data;
+        }
+        const std::vector<T>& getData() const { 
+            return data;
+        }
+        T& operator() (size_t i, size_t j) {
+            return data[j * m + i];
+        }
+        const T& operator() (size_t i, size_t j) const {
+            return data[j * m + i];
+        }
+    };
+} // namespace zagryadskov_a_matrix_sum
