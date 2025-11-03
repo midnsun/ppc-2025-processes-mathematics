51862c08a4e76abc4fa4feccfd5a9dbdac31910f
zagriadskov maksim <maksimzagradskrv50910@gmail.com>
2025-11-04 00:23:22 +0300

    Update max_by_column.cpp

--- tasks/zagryadskov_m_max_by_column/mpi/src/max_by_column.cpp
+++ tasks/zagryadskov_m_max_by_column/mpi/src/max_by_column.cpp
@@ -46,36 +46,52 @@ bool ZagryadskovMMaxByColumnMPI::RunImpl() {
   OutType &res = GetOutput();
   OutType local_res;
   OutType columns;
+  std::vector<int> sendcounts(world_size);
+  std::vector<int> displs(world_size);
+  displs[0] = 0;
   int columns_count = static_cast<int>(n) / world_size;
-  int columns_size = columns_count * static_cast<int>(m);
   using T = std::decay_t<decltype(*mat.begin())>;
   MPI_Datatype datatype = GetMpiType<T>();
   if (datatype == MPI_DATATYPE_NULL) {
     return false;
   }
 
-  columns.resize(columns_size);
   size_t i = 0;
   size_t j = 0;
+  int r = 0;
   T tmp = std::numeric_limits<T>::lowest();
   bool tmp_flag = false;
 
-  res.resize(n, std::numeric_limits<T>::lowest());
-  if (columns_size > 0) {
-    local_res.resize(columns_count, std::numeric_limits<T>::lowest());
-    MPI_Scatter(mat.data(), columns_size, datatype, columns.data(), columns_size, datatype, 0, MPI_COMM_WORLD);
+  res.assign(n, std::numeric_limits<T>::lowest());
+  if (columns_count > 0) {
+    for (r = 0; r < world_size; ++r) {
+      sendcounts[r] = (columns_count + static_cast<int>(r < (static_cast<int>(n) % world_size))) * static_cast<int>(m);
+      if (r > 0) {
+        displs[r] = displs[r - 1] + sendcounts[r - 1];
+      }
+    }
+    local_res.assign(static_cast<size_t>(sendcounts[world_rank]) / m, std::numeric_limits<T>::lowest());
+    columns.resize(sendcounts[world_rank]);
+    MPI_Scatterv(mat.data(), sendcounts.data(), displs.data(), datatype, columns.data(), sendcounts[world_rank],
+                 datatype, 0, MPI_COMM_WORLD);
 
-    for (j = 0; std::cmp_less(j, columns_count); ++j) {
+    for (j = 0; std::cmp_less(j, local_res.size()); ++j) {
       for (i = 0; i < m; ++i) {
         tmp = columns[(j * m) + i];
         tmp_flag = tmp > local_res[j];
         local_res[j] = (static_cast<T>(tmp_flag) * tmp) + (static_cast<T>(!tmp_flag) * local_res[j]);
       }
     }
-    MPI_Gather(local_res.data(), columns_count, datatype, res.data(), columns_count, datatype, 0, MPI_COMM_WORLD);
-  }
-  if (world_rank == 0) {
-    for (j = static_cast<size_t>(columns_count) * static_cast<size_t>(world_size); std::cmp_less(j, n); ++j) {
+    for (r = 0; r < world_size; ++r) {
+      sendcounts[r] /= static_cast<int>(m);
+      if (r > 0) {
+        displs[r] = displs[r - 1] + sendcounts[r - 1];
+      }
+    }
+    MPI_Gatherv(local_res.data(), static_cast<int>(local_res.size()), datatype, res.data(), sendcounts.data(),
+                displs.data(), datatype, 0, MPI_COMM_WORLD);
+  } else if (world_rank == 0) {
+    for (j = 0; std::cmp_less(j, n); ++j) {
       for (i = 0; i < m; ++i) {
         tmp = mat[(j * m) + i];
         tmp_flag = tmp > res[j];
